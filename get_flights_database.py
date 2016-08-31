e2ba672aa4d7c45a50326cbee8de327790febe2d
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2016-09-01 03:31:57 +0530

    Update get_flights_database.py

--- get_flights_database.py
+++ get_flights_database.py
@@ -3,7 +3,7 @@ import sys
 import time
 import json
 import MySQLdb
-db = MySQLdb.connect('localhost','root','BlackHole','yatra')
+db = MySQLdb.connect('localhost','root',*******,'yatra')
 cursor = db.cursor()
 origin = sys.argv[1]
 destination = sys.argv[2] 
