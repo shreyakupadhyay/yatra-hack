ee4db8f4dcefc38868cc9a061286c8f89c9e2849
Shreyak Upadhyay <shreyakupadhyay07@gmail.com>
2016-10-27 02:39:08 +0530

    Update get_id.py

--- get_id.py
+++ get_id.py
@@ -1,18 +1,14 @@
-import urllib
-import sys
-import json
-import os
-import time
+import urllib ,sys , json , os, time
+
 API_KEY = '*******************'
 
-html = urllib.urlopen('http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey='+API_KEY +'&term='+ sys.argv[1])
+response = urllib.urlopen('http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey='+API_KEY +'&term='+ sys.argv[1])
 
-htmltext = json.loads(html.read())
+responseText = json.loads(response.read())
 
-print htmltext 
-all_prf = len(htmltext)
+all_prf = len(responseText)
 info_origin = []
 for i in range(0,all_prf):
-    info_origin.append(htmltext[i]['value'])
-    info_origin.append(htmltext[i]['label'])
-    os.system('python /home/shreyakupadhyay/Documents/yatra-hackathon/get_flights.py ' + str(htmltext[i]['value']) + ' ' + 'BLR')
+    info_origin.append(responseText[i]['value'])
+    info_origin.append(responseText[i]['label'])
+    os.system('python /home/shreyakupadhyay/Documents/yatra-hackathon/get_flights.py ' + str(responseText[i]['value']) + ' ' + 'BLR')
